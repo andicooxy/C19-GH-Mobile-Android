1b62da75f08cc3737f2ed153b57b9b3fccf6437f
Your Name <amankwah.bonsu@nfortics.com>
2020-03-28 20:08:06 +0000

    Completed with login screen

---
+++ C19_Android/app/src/main/java/com/jamper/c19_android/ui/login/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.jamper.c19_android.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jamper.c19_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    
+}
